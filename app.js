2545a71308e0ca2401a402a694137c12ec79b2da
Larissa <96215793+imlari@users.noreply.github.com>
2023-05-14 10:24:14 -0300

    alteração na rota

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ cors = require('cors'),
 mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/mean', { useNewUrlParser: true, useUnifiedTopology: true }).then(
+mongoose.connect('mongodb://127.0.0.1:27017/mean', { useNewUrlParser: true, useUnifiedTopology: true }).then(
   () => {console.log('Database is connected') },
   err => { console.log('Can not connect to the database'+ err)});
 
